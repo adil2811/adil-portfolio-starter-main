97832139dfd3007ffc4488532cfe1daf6ffc0adb
adil2811 <128904178+adil2811@users.noreply.github.com>
2023-12-04 16:05:44 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,15 @@ const Header = () => {
   
   
   
-  <header className=" absoulte z-30 w-full item-center px-16 xl:px-0 xl-h-[90px]">
+  <header className=" absoulte z-30 w-full item-center px-20 xl:px-0 xl-h-[90px] pt-10 pb-5">
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row justfy-between items-center gap-y-6 py8 mt-3 xl:text-[40px]">
-        Adil  <span className="text-red-800 ml-2">  Shaikh</span>
+          <h1 className="text-red-800 ml-2 text-4xl">
+            <span className="text-white mr-2">
+            Adil 
+            </span>
+              Shaikh</h1>
+            
 
       </div>
 
